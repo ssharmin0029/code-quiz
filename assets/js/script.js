a6a816a1a727e50f1b4d0450cb9ea59452f4dc53
ssharmin0029 <sabrina.sharmin29@myhunter.cuny.edu>
2021-02-27 22:57:00 -0500

    js questions updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,3 +28,37 @@ var clearScoresButton = document.querySelector("#clear-scores-button");
 var scoreList = [];
 // ------------------------------------------------------------------------
 
+//
+var mcQuestions = [
+    {
+        questionTitle: "What does HTML stands for?",
+        choices: ["1. Hypertext Machine language", "2. Hypertext and links markup language", "3. Hypertext Markup Language", "4. Hightext machine language"],
+        answer: "3"
+    },
+    {
+        questionTitle: "What is the default value of the position property?",
+        choices: ["1. relative", "2. fixed", "3. absolute", "4. static"],
+        answer: "4"
+    },
+    {
+        questionTitle: "Commonly used data types DO NOT include:",
+        choices: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+        answer: "3"
+    },
+    {
+        questionTitle: "The condition in an if/else statement is enclosed within ________.",
+        choices: ["1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"],
+        answer: "3"
+    },
+    {
+        questionTitle: "The Document Object Model (DOM) allows you to:",
+        choices: ["1. Get elements from the DOM", "2. Create, Add and Remove elements in the DOM", "3. Traverse the elements of the DOM", "4. All of the above"],
+        answer: "4"
+    },
+    {
+        questionTitle: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["1. JavaScript", "2. terminal/bash", "3. for loops", "4. console.log"],
+        answer: "4"
+    }
+]
+// ------------------------------------------------------------------------
